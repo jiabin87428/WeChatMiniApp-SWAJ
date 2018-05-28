9a2a6c8640bf5a519348baaf8055baafc62cb57f
Andy <003jiabin@sina.com>
2018-05-29 00:31:58 +0800

    新建隐患页面

--- 代码/component/cells/imageSelectedCell.js
+++ 代码/component/cells/imageSelectedCell.js
@@ -4,20 +4,64 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    imageList: [],
+    littleImageWidth:0,
+    imageViewHeight:100
+  },
+  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+  attached: function () {
+    let screenWidth = wx.getSystemInfoSync().windowWidth
+    this.setData({
+      littleImageWidth: (screenWidth-50)/4
+    })
+    var _this = this;
+  },
+  moved: function () {
 
   },
+  detached: function () {
 
+  },
   /**
    * 组件的方法列表
    */
   methods: {
+    addPhoto: function() {
+      var _this = this;
+      wx.chooseImage({
+        success: function (res) {
+          _this.setData({
+            imageList: _this.data.imageList.concat(res.tempFilePaths),
+          })
 
+          _this.setData({
+            imageViewHeight: Math.ceil((_this.data.imageList.length + 1) / 4) * (_this.data.littleImageWidth + 8)
+          })
+
+          // wx.getImageInfo({
+          //   src: res.tempFilePaths[0],
+          //   success: function (res) {
+          //     console.log(res.width)
+          //     console.log(res.height)
+          //   }
+          // })
+        }
+      })
+    },
+    viewPhoto: function (e) {
+      var _this = this
+      var current = e.target.dataset.src; 
+      wx.previewImage({
+        current: current, // 当前显示图片的http链接
+        urls: _this.data.imageList // 需要预览的图片http链接列表
+      })
+    }
   }
 })
--- 代码/pages/danger/addDanger.js
+++ 代码/pages/danger/addDanger.js
@@ -1,18 +1,33 @@
 // pages/danger/addDanger.js
+// 在需要使用的js文件中，导入js  
+var util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    imageList: [],
+    littleImageWidth: 0,
+    imageViewHeight: 100,
+    time:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let screenWidth = wx.getSystemInfoSync().windowWidth
+    this.setData({
+      littleImageWidth: (screenWidth - 50) / 4
+    })
+
+    // 调用函数时，传入new Date()参数，返回值是日期和时间  
+    var time = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据  
+    this.setData({
+      time: time
+    });
   },
 
   /**
@@ -62,5 +77,47 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  addPhoto: function () {
+    var _this = this;
+    wx.chooseImage({
+      success: function (res) {
+        _this.setData({
+          imageList: _this.data.imageList.concat(res.tempFilePaths),
+        })
+
+        _this.setData({
+          imageViewHeight: Math.ceil((_this.data.imageList.length + 1) / 4) * (_this.data.littleImageWidth + 8)
+        })
+
+        // wx.getImageInfo({
+        //   src: res.tempFilePaths[0],
+        //   success: function (res) {
+        //     console.log(res.width)
+        //     console.log(res.height)
+        //   }
+        // })
+      }
+    })
+  },
+  viewPhoto: function (e) {
+    var _this = this
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接
+      urls: _this.data.imageList // 需要预览的图片http链接列表
+    })
+  },
+  deleteImage: function (e) {
+    var _this = this
+    var currentIdx = e.currentTarget.id;
+    var list = _this.data.imageList;
+    list.splice(currentIdx,1)
+    _this.setData({
+      imageList: list
+    })
+    _this.setData({
+      imageViewHeight: Math.ceil((_this.data.imageList.length + 1) / 4) * (_this.data.littleImageWidth + 8)
+    })
   }
 })
